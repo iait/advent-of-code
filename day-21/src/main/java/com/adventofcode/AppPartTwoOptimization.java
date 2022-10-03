c3884d6e10d56257f39be2f59dfd98c25abf00c1
Ismael Ait <IsmaelDavid.Ait@endava.com>
2022-10-03 09:46:12 -0300

    Add new solution day 21 using cache

--- day-21/src/main/java/com/adventofcode/AppPartTwoOptimization.java
+++ day-21/src/main/java/com/adventofcode/AppPartTwoOptimization.java
@@ -5,22 +5,28 @@ import java.util.Map;
 import java.util.Map.Entry;
 import java.util.TreeMap;
 
-public class AppPartTwo {
+public class AppPartTwoOptimization {
 
     private int calls = 0;
+    private int hits = 0;
 
     private static final int DICE_SIDES = 3;
     private static final int BOARD_SPACES = 10;
     private static final int WIN_SCORE = 21;
+    private static final int PLAYERS = 2;
 
     private final Map<Integer, Integer> outcomes;
 
+    // 10*21*10*21*2*8 bytes = 705,600 bytes = 690 KB
+    private final long[][][][][] cache =
+            new long[BOARD_SPACES][WIN_SCORE][BOARD_SPACES][WIN_SCORE][PLAYERS];
+
     public static void main(String[] args) {
 
-        new AppPartTwo().solve();
+        new AppPartTwoOptimization().solve();
     }
 
-    public AppPartTwo() {
+    public AppPartTwoOptimization() {
         var outcomes = computePossibleOutcomes();
         System.out.println("Sum frequencies:");
         for (Entry<Integer, Integer> entry : outcomes.entrySet()) {
@@ -37,8 +43,8 @@ public class AppPartTwo {
         System.out.println("Player 1 wins: " + wins[0]);
         System.out.println("Player 2 wins: " + wins[1]);
         System.out.println("Time: " + ((end - start) / 1_000_000) + "ms");
-
-        System.out.println("calls: " + calls);
+        System.out.println(
+                "calls: " + calls + ", hits: " + hits + ", misses: " + (calls - hits));
     }
 
     /**
@@ -54,7 +60,14 @@ public class AppPartTwo {
     private long[] computeWins(int turnPosition, int turnScore, int waitPosition, int waitScore) {
 
         calls++;
-        long[] wins = new long[2];
+        long[] winsCache =
+                cache[turnPosition - 1][turnScore][waitPosition - 1][waitScore];
+        if (winsCache[0] != 0L || winsCache[1] != 0L) {
+            hits++;
+            return winsCache;
+        }
+
+        long[] wins = new long[]{0, 0};
 
         for (Entry<Integer, Integer> entry : outcomes.entrySet()) {
             int outcome = entry.getKey();
@@ -69,6 +82,8 @@ public class AppPartTwo {
                 wins[1] += newWins[0] * freq;
             }
         }
+        winsCache[0] = wins[0];
+        winsCache[1] = wins[1];
         return wins;
     }
 
